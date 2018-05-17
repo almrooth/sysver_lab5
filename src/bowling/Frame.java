88ec284c18c45bc6c0dca8b8432e846e8560a9b3
Tobias Almroth <almrooth@gmail.com>
2018-05-17 13:19:41 +0200

    Refactored user story 1 with getters and setters

--- src/bowling/Frame.java
+++ src/bowling/Frame.java
@@ -6,15 +6,25 @@ public class Frame {
 	private int throw2;
 	
 	public Frame(int throw1, int throw2) {
-		this.throw1 = throw1;
-		this.throw2 = throw2;
+		this.setThrow1(throw1);
+		this.setThrow2(throw2);
 	}
-	
-	public int getThrow1()  {
+
+	public int getThrow1() {
 		return throw1;
 	}
-	
+
+	public void setThrow1(int throw1) {
+		this.throw1 = throw1;
+	}
+
 	public int getThrow2() {
 		return throw2;
 	}
+
+	public void setThrow2(int throw2) {
+		this.throw2 = throw2;
+	}
+	
+	
 }
