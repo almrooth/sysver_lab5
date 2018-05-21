eed16992d461b261bb9434a49000effd327040df
Tobias Almroth <almrooth@gmail.com>
2018-05-21 16:53:07 +0200

    Implemented user story 8.

--- src/bowling/Game.java
+++ src/bowling/Game.java
@@ -11,7 +11,11 @@ public class Game {
 		
 		for (int i = 0; i < frames.length; i++) {
 			if (frames[i].isStrike()) {
-				score += this.getStrikeScore(frames[i], frames[i+1]);
+				if (frames[i].isStrike() && frames[i+1].isStrike()) {
+					score += this.getMultiStrikeScore(frames[i], frames[i+1], frames[i+2]);
+				} else {
+					score += this.getStrikeScore(frames[i], frames[i+1]);
+				}				
 			} else if (frames[i].isSpare()) {
 				score += this.getSpareScore(frames[i], frames[i+1]);
 			} else {
@@ -27,5 +31,9 @@ public class Game {
 	
 	public int getSpareScore(Frame frame, Frame subFrame) {
 		return frame.getScore() + subFrame.throw1;
+	}
+	
+	public int getMultiStrikeScore(Frame frame, Frame frame2, Frame frame3) {
+		return frame.getScore() + frame2.getScore() + frame3.throw1;
 	}
 }
